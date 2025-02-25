36f650504d30c451eafca84cbf55a4c29b36e6fe
omuny <omuny@yandex.ru>
2025-02-26 02:46:47 +0300

    Создание простого дизайна приложения. Установка параметров и названия окна

--- src/main/java/com/weather/Controller.java
+++ src/main/java/com/weather/Controller.java
@@ -3,12 +3,51 @@ package com.weather;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Button getData;
+
+    @FXML
+    private Text pressure;
+
+    @FXML
+    private Text temp_feels;
+
+    @FXML
+    private Text temp_info;
+
+    @FXML
+    private Text temp_max;
+
+    @FXML
+    private Text temp_min;
+
+    @FXML
+    void initialize() {
+        assert city != null : "fx:id=\"city\" was not injected: check your FXML file 'Weather-view.fxml'.";
+        assert getData != null : "fx:id=\"getData\" was not injected: check your FXML file 'Weather-view.fxml'.";
+        assert pressure != null : "fx:id=\"pressure\" was not injected: check your FXML file 'Weather-view.fxml'.";
+        assert temp_feels != null : "fx:id=\"temp_feels\" was not injected: check your FXML file 'Weather-view.fxml'.";
+        assert temp_info != null : "fx:id=\"temp_info\" was not injected: check your FXML file 'Weather-view.fxml'.";
+        assert temp_max != null : "fx:id=\"temp_max\" was not injected: check your FXML file 'Weather-view.fxml'.";
+        assert temp_min != null : "fx:id=\"temp_min\" was not injected: check your FXML file 'Weather-view.fxml'.";
+
     }
+
 }
--- src/main/java/com/weather/Main.java
+++ src/main/java/com/weather/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Weather-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 250, 357);
+        stage.setTitle("Погодник");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
